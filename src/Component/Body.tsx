df21086c9b41ca1ddf567363b3ac5827c34f2b13
Md Shabbir Jamal <mdshabbir820141@gmail.com>
2025-11-02 22:38:47 +0530

    refactor: reponsive

--- src/Component/Body.tsx
+++ src/Component/Body.tsx
@@ -20,7 +20,7 @@ interface MethodDetails {
 type TrainingState = Record<string, MethodDetails>;
 
 const Body = () => {
-    const [method, setMethod] = useState<string>("");
+    const [method, setMethod] = useState<string>("lsvc");
     const [state, setState] = useState<TrainingState>({
         "lsvc": {isLoaded: false, runner: "run_prediction_lsvc"}
     });
@@ -35,23 +35,23 @@ const Body = () => {
     // }, [method]);
 
     return (
-        <div className = "w-full h-dvh grid grid-cols-8 gap-4 justify-items-center content-center items-center [background-size:40px_40px] [background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]">
-            <Input className = "text-input h-fit text-2xl col-start-2 col-span-4 shadow-shadow" type="text" id="text-input" placeholder="Enter some text"/>
+        <div className = "w-full h-dvh grid grid-cols-16 gap-3 md:grid-cols-8 md:gap-4 justify-items-center content-center items-center [background-size:40px_40px] [background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]">
+            <Input className = "h-fit text-sm lg:text-2xl col-start-2 col-span-8 self-end md:self-auto md:col-start-2 md:col-span-4 shadow-shadow" type="text" id="text-input" placeholder="Enter some text"/>
             <Select value={method} onValueChange={setMethod}>
-                <SelectTrigger className=" shadow-shadow w-[280px] col-span-2 text-xl h-fit flex-none ">
-                    <SelectValue placeholder="Select a Training Method" />
+                <SelectTrigger className="shadow-shadow w-[140px] md:w-[220px] lg:w-[280px] col-span-7 md:col-span-2 text-xs md:text-sm lg:text-xl h-fit flex-none ">
+                    <SelectValue placeholder="Select a Training Method"/>
                 </SelectTrigger>
                 <SelectContent>
                     <SelectGroup>
-                    <SelectLabel>Methodologies</SelectLabel>
-                    <SelectItem value="lsvc">LSVC with tfdif</SelectItem>
+                    <SelectLabel className='text-xs md:text-sm lg:text-xl'>Methodologies</SelectLabel>
+                    <SelectItem value="lsvc" className='text-xs md:text-sm lg:text-xl'>LSVC with tfdif</SelectItem>
                     </SelectGroup>
                 </SelectContent>
             </Select>
-            <ScrollArea id="emotion-output" className= "col-span-4 col-start-2 justify-self-end rounded-base h-fit w-[350px] text-xl border-2 border-border bg-main p-2 shadow-shadow">
+            <ScrollArea id="emotion-output" className= "col-start-2 col-span-8 md:col-span-4 md:col-start-2 justify-self-end rounded-base h-10 md:h-fit w-[150px] md:w-[250px] lg:w-[350px] text-xs md:text-sm lg:text-xl border-2 border-border bg-main p-2 shadow-shadow">
                 Emotion : 
             </ScrollArea>
-            <Button className = "text-xl w-[280px] col-span-2 hover:cursor-pointer" id="predict-button" py-click={state[method]?.runner}>Predict Emotion</Button>
+            <Button className = "text-xs md:text-sm lg:text-xl w-[140px] md:w-[220px] lg:w-[280px] col-span-7 md:col-span-2 hover:cursor-pointer" id="predict-button" py-click={state[method]?.runner}>Predict Emotion</Button>
 
         </div>
     );
--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -1,7 +1,22 @@
-function Footer(){
+import { RxExternalLink } from "react-icons/rx";
+import { Button } from "@/components/ui/button";
+
+const Footer = () => {
     return (
-        <div className = "absolute bottom-0 w-full h-auto pt-5 pb-5 text-center bg-background align-middle shadow-[0_-3px_0px_rgb(1_1_1)]">
-            <h3>Made by <a href = "https://github.com/jamal474" >jamal474</a></h3>
+        <div className = "absolute bottom-0 w-full h-auto pt-1 pb-1 md:pt-2 md:pb2 lg:pt-5 lg:pb-5 text-center bg-background align-middle shadow-[0_-3px_0px_rgb(1_1_1)]">
+            <div className = "hover:cursor-pointer flex items-center justify-center">
+                Made by
+                <a href = "https://shabbirjamal.com" className="ml-3">
+                    <Button variant="noShadow" className="lg:hidden text-xs h-6">
+                        <h3>Md Shabbir Jamal</h3>
+                        <RxExternalLink/>
+                    </Button>
+                    <Button variant="neutral" className="hidden lg:flex text-sm">
+                        <h3>Md Shabbir Jamal</h3>
+                        <RxExternalLink/>
+                    </Button>
+                </a>
+            </div>
         </div>
     );
 }
